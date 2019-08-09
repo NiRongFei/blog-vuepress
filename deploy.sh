0ba7aa390abaa3611c1145823d53a88b00b69b58
NiRongFei <fei332085762@aliyun.com>
2019-08-09 11:22:52 +0800

    提交部署

--- deploy.sh
+++ deploy.sh
@@ -20,5 +20,3 @@ git push -f git@github.com:NiRongFei/nirongfei.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
-cd -
