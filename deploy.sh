bdc2445c60faaa6d622df3f070cfde424cb77756
NiRongFei <fei332085762@aliyun.com>
2020-11-18 11:25:38 +0800

    ci: 调整集成部署配置地址

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+
 # 确保脚本抛出遇到的错误
 set -e
 
@@ -13,12 +14,12 @@ cd docs/.vuepress/dist
 
 git init
 git add -A
-git commit -m '提交部署'
+git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:NiRongFei/nirongfei.github.io.git master
+# git push -f git@github.com:NiRongFei/nirongfei.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:NiRongFei/blog.git master:gh-pages
 
 cd -
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // base: '/blog/',
+  base: '/blog/',
   theme: 'reco',
   title: '知行合一',
   description: '知之真切笃实处即是行 行之明觉精察处即是知',
